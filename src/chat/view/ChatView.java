0c71fbfec812cf27cdebef97454f169166183079
JordanBurnett1210 <jabur1210@gmail.com>
2015-11-05 09:11:34 -0700

    not quite sure.

--- src/chat/view/ChatView.java
+++ src/chat/view/ChatView.java
@@ -1,13 +1,32 @@
 package chat.view;
 
 import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
 
 public class ChatView
 {
+	private String windowMessage;
+	private ImageIcon chatIcon;
+	
+	public ChatView()
+	{
+		windowMessage = "Hello from your friendly chatbot:D";
+		chatIcon = new ImageIcon(getClass().getResource("images/Ichigo_Hollow_Mask_Close_Up.png"));
+		
+	}
+	
 	public String chatInput(String popupText)
 	{
-		String returnedText = JOptionPane.showInputDialog(null, popupText);
+		String returnedText = "";
+		
+		returnedText = JOptionPane.showInputDialog(null, popupText, windowMessage, JOptionPane.PLAIN_MESSAGE, chatIcon, null, "Answer here :)").toString();
+		
 		return returnedText;
+	}
+	
+	public void displayText(String displayText)
+	{
+		JOptionPane.showMessageDialog(null, displayText, windowMessage, JOptionPane.INFORMATION_MESSAGE, chatIcon);
 	}
 
 }
