430c7316dc822420ea7730ae976cb0f2b8ee11b7
JordanBurnett1210 <jabur1210@gmail.com>
2016-03-07 09:04:34 -0700

    Completed twitter constructor. for now.

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -1,6 +1,23 @@
 package chat.model;
 
+import twitter4j.*;
+import java.util.ArrayList;
+
+/**
+ * @version 0.2
+ * @author jbur0473
+ * Class for all twitter related aspects of chatbot.
+ */
+
 public class CTECTwitter
 {
-
+	//declaration
+	private ArrayList<Status> statusList;
+	private ArrayList<String> wordList;
+	
+	public CTECTwitter()
+	{
+		statusList = new ArrayList<Status>();
+		wordList = new ArrayList<String>();
+	}
 }
