a1afc08ce262f4f7fc68a3549559f7abd03f40a1
JordanBurnett1210 <jabur1210@gmail.com>
2016-03-07 09:17:33 -0700

    started method to send a tweet

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -4,7 +4,7 @@ import twitter4j.*;
 import java.util.ArrayList;
 
 /**
- * @version 0.2
+ * @version 0.2.5
  * @author jbur0473
  * Class for all twitter related aspects of chatbot.
  */
@@ -14,10 +14,18 @@ public class CTECTwitter
 	//declaration
 	private ArrayList<Status> statusList;
 	private ArrayList<String> wordList;
+	private Twitter chatbotTwitter;
 	
 	public CTECTwitter()
 	{
 		statusList = new ArrayList<Status>();
 		wordList = new ArrayList<String>();
+		chatbotTwitter = TwitterFactory.getSingleton();
 	}
+	
+	public void sendTweet(String tweet)
+	{
+		
+	}
+	
 }
